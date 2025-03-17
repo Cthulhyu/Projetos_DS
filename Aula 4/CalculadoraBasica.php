b9692536599aa06f0a3dded82fbc1fb19fddca9f
gabriel.boaventura <gabrielbonab@gmail.com>
2025-03-17 10:39:00 -0300

    Adicionando o que foi feito na aula 4 da matéria de Desenvolvimento de Sistemas

--- Aula 4/CalculadoraBasica.php
+++ Aula 4/CalculadoraBasica.php
@@ -1,21 +1,22 @@
-
-<!doctype html>
+<<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Calculaixon</title>
+    <title>CalcBasico</title>
 </head>
 <body>
-    <form method="post" action="resultado_calc.php">
-        X:<input type="number" name="x">
-        <br><br>
-        Y:<input type="number" name="y">
-        <br><br>
-        somar:<input type="submit" name="somar">
-    </form>
+<form method="post" action="Result_Calcbasico.php">
+    <ol>
+        <li>Soma</li>
+        <li>Subtração</li>
+        <li>Multiplicação</li>
+        <li>Divisão</li>
+    </ol>
+
+</form>
 
 </body>
 </html>
